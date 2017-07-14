a3a96b9426aaa97a163fc55995d71700ebe9b192
Peter Kappelt <kappelt.peter@gmail.com>
2017-07-14 23:00:51 +0200

    Requiring command line parameter "-s", version output

--- kCoAPSocket/src/net/kappelt/kCoAPSocket/kCoapSocketMain.java
+++ kCoAPSocket/src/net/kappelt/kCoAPSocket/kCoapSocketMain.java
@@ -11,24 +11,35 @@ import com.beust.jcommander.Parameter;
  *
  */
 public class kCoapSocketMain {
-	@Parameter(names = {"-p", "--port"}, description = "Port for the TCP socket to use, default 1505")
+	@Parameter(names = {"-p", "--port"}, description = "Port for the TCP socket to use, default 1505", help=true)
 	private Integer port = 1505;
 	
-	@Parameter(names = {"-s", "--secret"}, description = "PSK that is used to connect to the gateway")
+	@Parameter(names = {"-s", "--secret"}, description = "PSK that is used to connect to the gateway", required=true, help=true)
 	private String coapPSK = "";
 	
 	public static void main(String[] args) {
 		kCoapSocketMain main = new kCoapSocketMain();
 		
-		JCommander.newBuilder()
-			.addObject(main)
-			.build()
-			.parse(args);
+		JCommander cmdLine = JCommander.newBuilder().addObject(main).build();	
+		try{
+			cmdLine.parse(args);
+		}catch(Exception e){
+			System.err.println("Parameter error: " + e.getMessage());
+			cmdLine.usage();
+			System.exit(-1);
+		}
+		
+		System.out.println("kCoAPSocket V0.1-dev1");
+		System.out.println();
 		
 		main.startThreadHandler();
 	}
 	
 	public void startThreadHandler(){
+		if(coapPSK.length() != 16){
+			System.out.println("Warning: Your set PSK \"" + coapPSK + "\" doesn't seem to be a correct Tradfri-PSK. This could be a false alarm.");
+			System.out.println();
+		}
 		//run the background socket thread
 		Thread threadHandler = new Thread(new TcpServerThread(port, coapPSK));
 		threadHandler.start();
